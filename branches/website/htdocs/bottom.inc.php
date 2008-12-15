62a6c181cdad2b4beb9c9ebad83b88a2f06aff87
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2008-12-15 11:23:15 +0000

    Adding the website to SVN.  Initial import

---
+++ branches/website/htdocs/bottom.inc.php
@@ -0,0 +1,8 @@
+</div>
+</div>
+</div>
+<div id='statusbar'><strong><a href='http://sitracker.sourceforge.net/'>(C) 2008 Support Incident Tracker</a>
+</div>
+<p align='center'><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=160319" alt="SourceForge Logo" border="0" height="31" width="88"></a></p>
+</body>
+</html>
