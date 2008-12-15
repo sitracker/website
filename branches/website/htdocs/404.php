7932e8e3cb0482b8b63d3fd7e3070eba8032d25a
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2008-12-15 14:06:37 +0000

    Fix 404 page now I've moved sidebar into header

--- branches/website/htdocs/404.php
+++ branches/website/htdocs/404.php
@@ -2,9 +2,9 @@
 $title = str_replace("/", "", $_SERVER['REQUEST_URI']);
 ?>
 <h2>Error</h2>
-<p>Oops! It seems we can't find that file.</p
+<p>Oops! It seems we can't find that file.</p>
 <p>We've just moved to a new wiki so if you were accessing a wiki link, you can
-click <a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=<?=$title?>'>
+click <a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=<?php echo $title?>'>
 here</a> to find the link at the new wiki.</p>
 <p>If that doesn't work, please use the links at the top to find your way around.</p>
-<?php include 'side.inc.php'; include 'bottom.inc.php'; ?>
+<?php include 'bottom.inc.php'; ?>
