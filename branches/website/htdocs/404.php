c42e8a54faed05958864e5548d8256f7f7879131
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2008-12-16 23:23:46 +0000

    301 redir for all broken links with no . in the page name, (probably wiki links)

--- branches/website/htdocs/404.php
+++ branches/website/htdocs/404.php
@@ -1,10 +1,16 @@
 <?php include 'top.inc.php';
 $title = str_replace("/", "", $_SERVER['REQUEST_URI']);
+if (strpos($title, '.') === FALSE)
+{
+    header ('HTTP/1.1 301 Moved Permanently');
+    header ("Location: http://apps.sourceforge.net/mediawiki/sitracker/index.php?title={$title}");
+    exit;
+}
 ?>
 <h2>Error</h2>
 <p>Oops! It seems we can't find that file.</p>
 <p>We've just moved to a new wiki so if you were accessing a wiki link, you can
-click <a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=<?php echo $title?>'>
-here</a> to find the link at the new wiki.</p>
+<a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=<?php echo $title?>'>click 
+here to find the link at the new wiki</a>.</p>
 <p>If that doesn't work, please use the links at the top to find your way around.</p>
 <?php include 'bottom.inc.php'; ?>
