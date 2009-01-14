8b1b97ba2fb89fe3edfdda4d929222b244472455
Ivan Lucas <ivan@sitracker.org>
2009-01-14 14:26:29 +0000

    Update version number and download link

--- htdocs/side.inc.php
+++ htdocs/side.inc.php
@@ -1,9 +1,9 @@
 <div id='side'>
  <img src='http://sitracker.sourceforge.net/newlogo.png' alt='SiT logo' />
  <h2>Releases</h2>
- <h3>Latest release: v3.40 (21 November 2008)</h3>
- <a href='http://sourceforge.net/project/showfiles.php?group_id=160319'>
+ <h3>Latest release: v3.41 (17 December 2008)</h3>
+ <a href='/Download'>
  <img class='left' src='http://sitracker.sourceforge.net/download.png'  style='padding-top:5px;' alt='' /><br />
  <strong>Download</strong><br />Download the latest release</a>
- <p>See the <a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=ReleaseNotes'>release notes</a> for details.</p>
+ <p>See the <a href='/ReleaseNotes'>release notes</a> for details.</p>
 </div>
