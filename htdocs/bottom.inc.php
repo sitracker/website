9278e9b3796bd25dc4ad4f390e64fa774803a45f
Ivan Lucas <ivan@sitracker.org>
2009-01-14 14:22:04 +0000

    Add a donation button and the salford software logo (and links) to the homepage

--- htdocs/bottom.inc.php
+++ htdocs/bottom.inc.php
@@ -1,6 +1,10 @@
 </div>
 </div>
 <div id='statusbar'><strong><a href='http://sitracker.sourceforge.net/'>&copy; 2008 Support Incident Tracker</a></strong></div>
-<p align='center'><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=160319" alt="SourceForge Logo" border="0" height="31" width="88" /></a></p>
+<p align='center'>
+ <a href="http://sourceforge.net/donate/index.php?group_id=160319"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" title="Support This Project" /></a> &nbsp;
+ <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=160319" alt="SourceForge Logo" title="Project hosting by Sourceforge.net" border="0" height="31" width="88" /></a> &nbsp;
+ <a href="http://www.salfordsoftware.co.uk/"><img src="salford-software-button.png" alt="Salford Software" title="Salford Software is proud to sponsor SiT development" border="0" height="31" width="86" /></a>
+</p>
 </body>
 </html>
