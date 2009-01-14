b6884270464e8b82dfe3f2f0dc2edf6e941591cb
Ivan Lucas <ivan@sitracker.org>
2009-01-14 14:29:47 +0000

    update Copyright date

--- htdocs/bottom.inc.php
+++ htdocs/bottom.inc.php
@@ -1,6 +1,6 @@
 </div>
 </div>
-<div id='statusbar'><strong><a href='http://sitracker.sourceforge.net/'>&copy; 2008 Support Incident Tracker</a></strong></div>
+<div id='statusbar'><strong><a href='http://sitracker.sourceforge.net/'>&copy; 2008-<?php echo date('Y'); ?> Support Incident Tracker</a></strong></div>
 <p align='center'>
  <a href="http://sourceforge.net/donate/index.php?group_id=160319"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" title="Support This Project" /></a> &nbsp;
  <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=160319" alt="SourceForge Logo" title="Project hosting by Sourceforge.net" border="0" height="31" width="88" /></a> &nbsp;
