ea34e3850fa7f9c765b0d386d2a0116a0868d117
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2009-01-22 15:35:50 +0000

    Added a script 'fetchnews.php' which, when run from your local workstation will update news and release info from RSS feeds, this then needs to be published to the website

---
+++ branches/website/htdocs/fetchnews.php
@@ -0,0 +1,38 @@
+<?php
+// Fetch news, from sit blog
+// Must be run from a machine that isn't sourceforge
+
+// Note: we can't use Magpie on sourceforge because they helpfully block all
+// outbound connectivity
+// see http://alexandria.wiki.sourceforge.net/Project+Web%2C+Shell%2C+VHOST+and+Database+Services#tocProject%20Web,%20Shell,%20VHOST%20and%20Database%20Services20
+
+include 'magpie/rss_fetch.inc';
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+$newsrss = fetch_rss('http://sitracker.wordpress.com/category/news/feed/');
+$relrss = fetch_rss('http://sourceforge.net/export/rss2_projnews.php?group_id=160319');
+
+$html = '';
+foreach ($newsrss->items AS $item)
+{
+    $item['description'] = str_replace('[...]', "[...] <a href='{$item['link']}'>Read more&hellip;</a>", $item['description']);
+    $html .= "\n<h3 class='headline'>{$item['title']}</h3>\n";
+    $html .= "<p class='underheadline'>Published {$item['pubdate']} by {$item['dc']['creator']}.  (<a href='{$item['link']}'>Permalink</a>)</p>";
+    $html .= "<p class='newsstory'>{$item['description']}</p>\n";
+}
+
+file_put_contents('news.html', $html);
+echo "news.html updated\n";
+
+
+$html = '';
+
+$version = str_replace('Support Incident Tracker (SiT!) ','',$relrss->items[0]['title']);
+$version = str_replace(' - Released','',$version);
+$reldate = date('d F Y',strtotime($relrss->items[0]['pubdate']));
+
+$html .= "<h3>Latest release: {$version} ({$reldate})</h3>\n";
+file_put_contents('release.html', $html);
+echo "release.html updated\n";
+
+?>
