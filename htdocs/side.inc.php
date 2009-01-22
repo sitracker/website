6698dbad3d5ff39d8411626d4d65f0227b2b9e7c
Ivan Lucas <ivan@sitracker.org>
2009-01-22 15:35:50 +0000

    Added a script 'fetchnews.php' which, when run from your local workstation will update news and release info from RSS feeds, this then needs to be published to the website

--- htdocs/side.inc.php
+++ htdocs/side.inc.php
@@ -1,8 +1,14 @@
 <div id='side'>
  <img src='http://sitracker.sourceforge.net/newlogo.png' alt='SiT logo' />
  <h2>Releases</h2>
- <h3>Latest release: v3.41 (17 December 2008)</h3>
- <a href='/Download'>
+<?php
+if (file_exists('release.html'))
+{
+    echo file_get_contents('release.html');
+}
+// <h3>Latest release: v3.41 (17 December 2008)</h3>
+?>
+<a href='/Download'>
  <img class='left' src='http://sitracker.sourceforge.net/download.png'  style='padding-top:5px;' alt='' /><br />
  <strong>Download</strong><br />Download the latest release</a>
  <p>See the <a href='/ReleaseNotes'>release notes</a> for details.</p>
--- htdocs/top.inc.php
+++ htdocs/top.inc.php
@@ -5,6 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 <title>SiT! Support Incident Tracker</title>
 <link rel='SHORTCUT ICON' href='http://sitracker.sourceforge.net/sit_favicon.png' />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://sitracker.wordpress.com/category/news/feed/" />
 <script src='http://sitracker.sourceforge.net/prototype.js' type='text/javascript'></script>
 <style type='text/css'>@import url('http://sitracker.sourceforge.net/sit.css');</style>
 </head>
