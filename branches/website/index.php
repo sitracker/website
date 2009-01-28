50581018bd6119004929bfbfcc3b3be0f398e3a1
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2009-01-28 11:25:59 +0000

    * Make news and releases RSS feeds refresh automatically using magpie * Add RSS icons/links for both feeds * Move the ohloh button to the sidebar

--- branches/website/index.php
+++ branches/website/index.php
@@ -1,14 +1,36 @@
 <?php
+$newsfeedurl = 'http://sitracker.wordpress.com/category/news/feed/';
+// NUmber of news items to show
+$newsitems = 4;
+
+$relfeedurl = 'http://sourceforge.net/export/rss2_projnews.php?group_id=160319';
+
+
 include 'top.inc.php';
+include_once 'magpie/rss_fetch.inc';
 ?>
 <h2>Welcome</h2>
 <p>Support Incident Tracker (or SiT!) is a <a href="http://www.gnu.org/philosophy/free-sw.html" target="_blank">Free Software</a>/Open Source (<a href="http://www.opensource.org/licenses/gpl-license.php" target="_blank">GPL</a>) web based application which uses <a href="http://www.php.net/" target="_blank">PHP</a> and <a href="http://www.mysql.com/" target="_blank">MySQL</a> for tracking technical support calls/emails (also commonly known as a 'Help Desk' or 'Support Ticket System'). Manage contacts, sites, technical support contracts and support incidents in one place. Send emails directly from SiT!, attach files and record every communication in the incident log. SiT is aware of Service Level Agreements and incidents are flagged if they stray outside of them.</p>
 <img class='pretty' src='http://sitracker.sourceforge.net/screenshots.jpg' alt='SiT Screenshot' />
-<h2>News</h2>
 <?php
-if (file_exists('news.html'))
+echo "<h2><div style='float:right;margin-top:6px;'><a href='{$newsfeedurl}' title='News feed (RSS)'>";
+echo "<img src='feed-icon-12x12.png' width='12' height='12' alt='' /></a></div>News</h2>";
+$newsrss = @fetch_rss($newsfeedurl);
+if (is_object($newsrss))
 {
-    echo utf8_encode(file_get_contents('news.html'));
+    $html = '';
+    $itemcount = 1;
+    foreach ($newsrss->items AS $item)
+    {
+        $item['description'] = str_replace('[...]', "[...] <a href='{$item['link']} class='more'>Read more&hellip;</a>", $item['description']);
+        $html .= "\n<h3 class='headline'>{$item['title']}</h3>\n";
+        $html .= "<p class='underheadline'>Published {$item['pubdate']} by {$item['dc']['creator']}.  (<a href='{$item['link']}' rel='bookmark'>Permalink</a>)</p>";
+        $html .= "<p class='newsstory'>{$item['description']}</p>\n";
+        $itemcount++;
+        if ($itemcount > $newsitems) break;
+    }
+    echo utf8_encode($html);
+    echo "<p><a href='http://sitracker.wordpress.com/category/news/'>Older news</a></p>";
 }
 else
 {
