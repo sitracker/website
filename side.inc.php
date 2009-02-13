e45cb347d4b00b32e403c359dc9073f414222130
kieran_hogg <xerosis@users.noreply.github.com>
2009-02-13 16:40:07 +0000

    add identi.ca feed to sidebar

--- side.inc.php
+++ side.inc.php
@@ -24,6 +24,27 @@ if (is_object($relrss))
  <img class='left' src='http://sitracker.sourceforge.net/download.png'  style='padding-top:5px;' alt='' /><br />
  <strong>Download</strong><br />Download the latest release</a>
  <p>See the <a href='/wiki/ReleaseNotes'>release notes</a> for details.</p>
+<?php
+echo "<h2><span style='float:right;margin-top:6px;'><a href='{$microblogfeedurl}' title='Identi.ca (RSS)'>";
+echo "<img src='feed-icon-12x12.png' width='12' height='12' alt='' /></a></span>";
+echo "<a href='http://identi.ca/groups/sit'>Identi.ca</a></h2>";
+
+$mbrss = fetch_rss($microblogurl);
+$count = 1;
+foreach ($mbrss->items as $post) 
+{
+    if ($count <= 3)
+    {
+        $post['dc']['date'] = str_replace("T", " @ ", $post['dc']['date']);
+        $post['dc']['date'] = str_replace("+00:00", "", $post['dc']['date']);
+        echo "<p>{$post['title']}<br />";
+        echo "<small><a href='{$post['link']}'>{$post['dc']['date']}</a></small></p>";
+        $count++;
+    }
+}
+?>
 <!-- Ohloh, I use it button -->
 <script type="text/javascript" src="http://www.ohloh.net/p/15567/widgets/project_users_logo.js"></script>
 </div>
+
+
