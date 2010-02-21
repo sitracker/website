f08696c773ae44656f0c229ebca5fb752f660804
ivanlucas <ivanlucas@6e0da8f0-d90d-0410-b14f-823b0ad1652c>
2010-02-21 09:50:29 +0000

    Just have the tsohost button only at the bottom of the page

--- branches/website/bottom.inc.php
+++ branches/website/bottom.inc.php
@@ -1,12 +1,8 @@
 </div>
 </div>
 <div id='statusbar'><strong><a href='http://sitracker.sourceforge.net/'>&copy; 2008-<?php echo date('Y'); ?> Support Incident Tracker</a></strong></div>
-<p align='center'>
- <a href="http://sourceforge.net/donate/index.php?group_id=160319"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" title="Support This Project" /></a> &nbsp;
- <a href="http://www.tsohost.co.uk/"><img src="tsohost-button.png" alt="Tsohost Logo" title="Hosted by Tsohost.co.uk clustered grid
-hosting" border="0" height="32" width="100" /></a> &nbsp;
- <a href="http://www.salfordsoftware.co.uk/"><img src="salford-software-button.png" alt="Salford Software" title="Salford Software is proud to sponsor SiT development" border="0" height="31" width="86" /></a>
-</p>
+<p align='center'> <a href="http://www.tsohost.co.uk/"><img src="tsohost-button.png" alt="Tsohost Logo" title="Hosted by Tsohost.co.uk clustered grid
+hosting" border="0" height="32" width="100" /></a></p>
 <p id='hostedby'>Hosted by <a href='http://www.tsohost.co.uk'>Tsohost.co.uk</a> clustered grid hosting</a></p>
 <!-- Piwik -->
 <a href="http://piwik.org" title="Open source web analytics" onclick="window.open(this.href);return(false);">
