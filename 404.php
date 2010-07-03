c03ef3ae0dc6da143da4c4a76262d1cdeaa5e375
Ivan Lucas <ivan@sitracker.org>
2010-07-03 15:28:57 +0000

    Fix the 404 page so it redirects to the wiki properly

--- 404.php
+++ 404.php
@@ -1,16 +1,20 @@
-<?php include 'top.inc.php';
+<?php
 $title = str_replace("/", "", $_SERVER['REQUEST_URI']);
 if (strpos($title, '.') === FALSE)
 {
     header ('HTTP/1.1 301 Moved Permanently');
-    header ("Location: http://apps.sourceforge.net/mediawiki/sitracker/index.php?title={$title}");
+    header ("Location: http://sitracker.org/wiki/{$title}");
     exit;
 }
-?>
-<h2>Error</h2>
-<p>Oops! It seems we can't find that file.</p>
-<p>We've just moved to a new wiki so if you were accessing a wiki link, you can
-<a href='http://apps.sourceforge.net/mediawiki/sitracker/index.php?title=<?php echo $title?>'>click 
-here to find the link at the new wiki</a>.</p>
-<p>If that doesn't work, please use the links at the top to find your way around.</p>
-<?php include 'bottom.inc.php'; ?>
+else
+{
+    include 'top.inc.php';
+    ?>
+    <h2>Error</h2>
+    <p>Oops! It seems we can't find that file.</p>
+    <p>We've just moved to a new wiki so if you were accessing a wiki link, you can
+    <a href='http://sitracker.org/wiki/<?php echo $title?>'>click
+    here to find the link at the new wiki</a>.</p>
+    <p>If that doesn't work, please use the links at the top to find your way around.</p>
+    <?php include 'bottom.inc.php';
+}
